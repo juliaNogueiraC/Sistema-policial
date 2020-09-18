4771892a5d3093efb29291e5bac229ab9f4a1e45
Júlia Nogueira <69528739+juliaNogueiraC@users.noreply.github.com>
2020-09-18 10:23:24 -0300

    iniciando implementação

---
+++ pega_ladrao/src/main/java/sistema_policial/pega_ladrao/PegaLadraoApplication.java
@@ -0,0 +1,13 @@
+package sistema_policial.pega_ladrao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PegaLadraoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PegaLadraoApplication.class, args);
+	}
+
+}
---
+++ pega_ladrao/src/test/java/sistema_policial/pega_ladrao/PegaLadraoApplicationTests.java
@@ -0,0 +1,13 @@
+package sistema_policial.pega_ladrao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PegaLadraoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
