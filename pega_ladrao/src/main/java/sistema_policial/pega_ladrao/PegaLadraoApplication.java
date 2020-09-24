ed92955af8f309c51ea0fdb6d294ca4be5f203c4
Júlia Nogueira <69528739+juliaNogueiraC@users.noreply.github.com>
2020-09-24 14:42:18 -0300

    service,controller,repository.

--- pega_ladrao/src/main/java/sistema_policial/pega_ladrao/PegaLadraoApplication.java
+++ pega_ladrao/src/main/java/sistema_policial/pega_ladrao/PegaLadraoApplication.java
@@ -1,13 +1,24 @@
 package sistema_policial.pega_ladrao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import sistema_policial.pega_ladrao.services.OcorrenciaService;
+
 @SpringBootApplication
 public class PegaLadraoApplication {
+
+
+	@Autowired
+	private OcorrenciaService service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PegaLadraoApplication.class, args);
 	}
 
+	@Override
+	 public void run(String... args)throws Exception {
+	 
+		System.out.println(service.lista()).size());// listagem do service 
 }
